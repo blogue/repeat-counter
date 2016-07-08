fff64480da31f659ff19ad6d1c1e49f9dbf2bf8c
Ben Logue <benjamin.logue73@gmail.com>
2016-07-08 08:35:42 -0700

    add object RepeatCounter with method to evaluate instances of word in sentence.

--- Objects/word-repeat-counter.cs
+++ Objects/word-repeat-counter.cs
@@ -6,6 +6,22 @@ namespace WordRepeatCounter
 {
   public class RepeatCounter
   {
+    private static List<string> _userStrings = new List<string>{};
 
+    public int CountRepeats(string userString, string userWord)
+    {
+      int repeatCounter = 0;
+      char[] delimCharacters = {' ', ',', '.', '!', '?', ':', ';'};
+      string[] userStringArray = userString.Split(delimCharacters);
+
+      for(int i=0; i<userStringArray.Length; i++)
+      {
+        if(userStringArray[i].ToLower().Contains(userWord.ToLower()))
+        {
+          repeatCounter++;
+        }
+      }
+      return repeatCounter;
+    }
   }
 }
